685ba03c1f7a7d453866831be462afe2f6f55599
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-04 23:40:47 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="card" style={{ width: " 18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 }
 
 export default App;
