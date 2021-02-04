a51a79502b4174b594803c7d5d7cc535bd4114d9
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-05 00:10:56 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      welcomeMsg = "xrissy pizza"
+    };
+  }
+
+  render() {
+    const welcomeMsg = this.state;
+    const greet = () => welcomeMsg.greet
+  }
+
+  return() {
+    <div>
+      <h1>hello steve {this.welcomeMsg.greet}</h1>
+    </div>;
   }
 }
 
